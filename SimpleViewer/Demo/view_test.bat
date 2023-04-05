043509109e5805a1ee7947e0e372e634201d1004
Zenny Chen <zenny_chen@sina.com>
2023-04-06 01:17:33 +0800

    Add files via upload

---
+++ SimpleViewer/Demo/view_test.bat
@@ -0,0 +1,9 @@
+echo 'Press g, f, s, w'
+..\install\bin\SimpleViewer.exe head.uv_nor.obj
+
+echo 'Press g, f, s, w,t'
+..\install\bin\SimpleViewer.exe Alex.uv.m ..\Textures\check_1k_64.bmp
+
+echo 'Press g, f, s, w,t'
+..\install\bin\SimpleViewer.exe Sophie.remesh.uv.m ..\Textures\check_1k_64.bmp
+
